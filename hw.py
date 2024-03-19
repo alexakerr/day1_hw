4106c8dfadc2c81633f6d193ff6aca662d63f43e
alexakerr <alexakerr996@gmail.com>
2024-03-19 10:13:18 -0400

    first commit

---
+++ hw.py
@@ -0,0 +1,52 @@
+# Problem 1 - Task One
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Problem 1 - Task Two
+mood = input("How do you feel today? ")
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
+
+#Problem 2 - Task One
+Pi_value = 3.14
+userAge = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+# Problem 3 - Task 1
+#variable_a is a string
+variable_a = "Hello, World!"
+#variable_b is a integer
+variable_b = 23
+#variable_c is a float
+variable_c = 3.14
+#variable_d is a boolean 
+variable_d = True
+
+
+#Problem 4 - Task 1
+bread = 2
+peanutbutter = 3
+jelly = 4
+pb_and_j = int(bread + peanutbutter + jelly)
+print(pb_and_j)
+
+#Problem 4 - Task 2
+savings = 10000
+interest_rate = 0.07
+amount_of_years = 1
+total_amount = float(savings * (amount_of_years + interest_rate))
+print(total_amount)
+
+#Problem 5 - Task 1
+a=1
+b=2
+a=2
+b=1
+if a>b:
+    print("2 is greater than 1")  
